56366ae82710193ef45bea40d8d13c25c3a398eb
Rodrigo Castro <rodrigocastro101998@gmail.com>
2025-07-30 20:57:18 -0400

    feat: fight recognition

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,2 +1,1 @@
-# __init__.py
 # Este archivo indica que este directorio debe ser tratado como un paquete Python.
--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import time
 import mss
 import numpy as np
 import cv2
-from bot.actions import ejecutar_movimientos
+from bot.actions import execute_movement, on_combat
 
 print("Cambiando a la ventana del juego... Capturando en 5 segundos.")
 time.sleep(5)
 
-# Captura de pantalla
 with mss.mss() as sct:
     screenshot = sct.grab(sct.monitors[1])
     img = np.array(screenshot)
 
-# Si se desea ver la imagen antes de comenzar a procesar
 cv2.imshow("Screen", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Empezamos el loop de captura de pantalla
-while True:
+attempts = 0
+detected_combat = False
+
+while not detected_combat:
+    attempts += 1
+    print(f"Intento número: {attempts}")
+
+    execute_movement()
+
     with mss.mss() as sct:
         screenshot = sct.grab(sct.monitors[1])
         img = np.array(screenshot)
-
-    # Ejecutar movimientos y lógica del bot
-    if ejecutar_movimientos(img):
-        break  # Detener si encontramos un Pokémon shiny
+    detected_combat = on_combat(img)
+    time.sleep(1)
